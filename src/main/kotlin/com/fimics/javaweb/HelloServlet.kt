e3a162f0f7f19b85471e6bdf4d865344c928311b
fimics <lpjhblpj@163.com>
2021-07-17 17:00:14 +0800

    init

---
+++ src/main/kotlin/com/fimics/javaweb/HelloServlet.kt
@@ -0,0 +1,28 @@
+package com.fimics.javaweb
+
+import javax.servlet.http.*
+import javax.servlet.annotation.*
+
+//@WebServlet(name = "helloServlet", value = ["/hello-servlet"])
+//@WebServlet(urlPatterns = ["/helloServlet"])
+@WebServlet("/helloServlet")
+class HelloServlet : HttpServlet() {
+    private lateinit var message: String
+
+    override fun init() {
+        message = "Hello World!"
+    }
+
+    public override fun doGet(request: HttpServletRequest, response: HttpServletResponse) {
+        response.contentType = "text/html"
+
+        // Hello
+        val out = response.writer
+        out.println("<html><body>")
+        out.println("<h1>$message</h1>")
+        out.println("</body></html>")
+    }
+
+    override fun destroy() {
+    }
+}
